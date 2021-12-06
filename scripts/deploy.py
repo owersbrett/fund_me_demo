9d2728d246438c57fd51420a2fc764bfb1582aeb
bretto.eth <bretto.eth@gmail.com>
2021-12-05 21:44:11 -0700

    fund me contract initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,26 @@
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful_scripts import deploy_mocks, get_account
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+    active_network = network.show_active()
+    development_network_active = active_network == "development"
+
+    if development_network_active:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+    else:
+        price_feed_address = config["networks"][active_network]["eth_usd_price_feed"]
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][active_network].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
